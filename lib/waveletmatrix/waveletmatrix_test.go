bfa45af5c01aec9d272fbf29d0e84623431c976b
ynzwtks <73768325+ynzwtks@users.noreply.github.com>
2023-04-12 22:07:24 +0900

    retry
    retry

--- lib/waveletmatrix/waveletmatrix_test.go
+++ lib/waveletmatrix/waveletmatrix_test.go
@@ -1,82 +1,151 @@
 package waveletmatrix
 
 import (
+	"math/rand"
+	"sort"
 	"testing"
 )
 
-func Test_waveletmatrix(t *testing.T) {
-	a := []int{4, 5, 5, 5, 2, 1, 5, 6, 1, 3, 5, 0}
-	wm := NewWaveletMatrix(a)
-
-	b := []int{1, 10, 100, 1000, 1000, 100, 10, 1}
-	wm2 := NewWaveletMatrix(b)
-
-	//Access
-	if r := wm.Access(0); r != int(4) {
-		t.Error("Expected", 4, "Got", r)
-	}
-	if r := wm.Access(6); r != int(5) {
-		t.Error("Expected", 5, "Got", r)
-	}
-	if r := wm.Access(11); r != int(0) {
-		t.Error("Expected", 0, "Got", r)
-	}
-	if r := wm.Access(9); r != int(3) {
-		t.Error("Expected", 3, "Got", r)
-	}
-	if r := wm2.Access(0); r != int(1) {
-		t.Error("Expected", 1, "Got", r)
-	}
-	if r := wm2.Access(3); r != int(1000) {
-		t.Error("Expected", 1000, "Got", r)
-	}
-	if r := wm2.Access(7); r != int(1) {
-		t.Error("Expected", 1, "Got", r)
-	}
-	//Rank
-	if r := wm.Rank(5, 3); r != int(3) {
-		t.Error("Expected", 3, "Got", r)
-	}
-	if r := wm.Rank(5, 1); r != int(1) {
-		t.Error("Expected", 1, "Got", r)
-	}
-	if r := wm.Rank(1, 8); r != int(2) {
-		t.Error("Expected", 2, "Got", r)
+func TestNewWaveletMatrix(t *testing.T) {
+	testCases := [][]int{
+		{},
+		{1, 1, 1, 1},
+		{1, 2, 3, 4, 5},
+		{5, 4, 3, 2, 1},
 	}
 
-	if r := wm2.Rank(1, 7); r != int(2) {
-		t.Error("Expected", 2, "Got", r)
+	for _, testCase := range testCases {
+		_ = NewWaveletMatrix(testCase)
 	}
-	if r := wm2.Rank(100, 4); r != int(1) {
-		t.Error("Expected", 1, "Got", r)
+}
+
+func TestAccess(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5}
+	wm := NewWaveletMatrix(data)
+
+	for i, v := range data {
+		if wm.Access(i) != v {
+			t.Errorf("Access(%d) returned %d, expected %d", i, wm.Access(i), v)
+		}
 	}
-	//Select
-	if r, _ := wm.Select(5, 4); r != int(6) {
-		t.Error("Expected", 6, "Got", r)
+}
+
+func TestSelect(t *testing.T) {
+	data := []int{1, 2, 2, 3, 3, 3}
+	wm := NewWaveletMatrix(data)
+
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= i; j++ {
+			pos, ok := wm.Select(i, j)
+			if !ok || data[pos] != i {
+				t.Errorf("Select(%d, %d) returned %d, expected %d", i, j, pos, (i-1)*i+j-1)
+			}
+		}
 	}
-	if r, _ := wm.Select(0, 1); r != int(11) {
-		t.Error("Expected", 11, "Got", r)
+
+	_, ok := wm.Select(4, 1)
+	if ok {
+		t.Error("Select(4, 1) should return false")
 	}
-	if r, _ := wm.Select(1, 2); r != int(8) {
-		t.Error("Expected", 8, "Got", r)
+}
+
+func TestRank(t *testing.T) {
+	data := []int{1, 2, 1, 2, 1, 2}
+	wm := NewWaveletMatrix(data)
+
+	type query struct {
+		n, idx, want int
 	}
-	if r, _ := wm2.Select(1, 2); r != int(7) {
-		t.Error("Expected", 7, "Got", r)
+
+	queries := []query{
+		{1, 0, 1},
+		{1, 1, 1},
+		{1, 2, 2},
+		{1, 3, 2},
+		{2, 0, 0},
+		{2, 1, 1},
+		{2, 2, 1},
+		{2, 3, 2},
+		{2, 4, 2},
+		{2, 5, 3},
 	}
-	if r, _ := wm2.Select(1, 1); r != int(0) {
-		t.Error("Expected", 0, "Got", r)
+
+	for _, q := range queries {
+		got := wm.Rank(q.n, q.idx)
+		if got != q.want {
+			t.Errorf("Rank(%d, %d) returned %d, expected %d", q.n, q.idx, got, q.want)
+		}
 	}
-	//Quantile
-	if r := wm.Quantile(1, 10, 8); r != int(5) {
-		t.Error("Expected", 5, "Got", r)
+}
+
+func TestQuantile(t *testing.T) {
+	data := []int{1, 2, 2, 3, 3, 3}
+	wm := NewWaveletMatrix(data)
+
+	quantiles := []int{1, 2, 2, 3, 3, 3}
+	for i, q := range quantiles {
+		if wm.Quantile(0, 5, i+1) != q {
+			t.Errorf("Quantile(0, 5, %d) returned %d, expected %d", i+1, wm.Quantile(0, 5, i+1), q)
+		}
 	}
-	if r := wm.Quantile(0, 1, 1); r != int(4) {
-		t.Error("Expected", 4, "Got", r)
+}
+func TestQuantile2(t *testing.T) {
+	data := []int{1, 2, 2, 3, 3, 3}
+	wm := NewWaveletMatrix(data)
+
+	quantiles2 := []int{3, 3, 3, 2, 2, 1}
+	for i, q := range quantiles2 {
+		if wm.Quantile2(0, 5, i+1) != q {
+			t.Errorf("Quantile2(0, 5, %d) returned %d, expected %d", i+1, wm.Quantile2(0, 5, i+1), q)
+		}
 	}
-	if r := wm2.Quantile(0, 7, 1); r != int(1) {
-		t.Error("Expected", 1, "Got", r)
+}
+
+func TestWaveletMatrixRandomData(t *testing.T) {
+	data := make([]int, 1000)
+	for i := range data {
+		data[i] = rand.Intn(1000)
 	}
-	if r := wm2.Quantile(1, 3, 1); r != int(10) {
-		t.Error("Expected", 10, "Got", r)
+
+	wm := NewWaveletMatrix(data)
+	sortedData := make([]int, 1000)
+	copy(sortedData, data)
+	sort.Ints(sortedData)
+
+	for i := 0; i < 1000; i++ {
+		q := wm.Quantile(0, 999, i+1)
+		if q != sortedData[i] {
+			t.Errorf("Quantile(0, 999, %d) returned %d, expected %d", i+1, q, sortedData[i])
+		}
+
+		q2 := wm.Quantile2(0, 999, i+1)
+		if q2 != sortedData[999-i] {
+			t.Errorf("Quantile2(0, 999, %d) returned %d, expected %d", i+1, q2, sortedData[999-i])
+		}
+	}
+}
+func TestRangeFreq(t *testing.T) {
+	data := []int{0, 1, 2, 3, 2, 2, 1}
+	wm := NewWaveletMatrix(data)
+
+	tests := []struct {
+		l, r, x int
+		want    int
+	}{
+		{l: 0, r: 6, x: 2, want: 3},
+		{l: 1, r: 6, x: 2, want: 3},
+		{l: 2, r: 6, x: 2, want: 3},
+		{l: 3, r: 6, x: 2, want: 2},
+		{l: 0, r: 6, x: 0, want: 1},
+		{l: 0, r: 6, x: 1, want: 2},
+		{l: 0, r: 6, x: 3, want: 1},
+		{l: 4, r: 4, x: 5, want: 0},
+	}
+
+	for _, test := range tests {
+		result := wm.RangeFreq(test.l, test.r, test.x)
+		if result != test.want {
+			t.Errorf("RangeFreq(%d, %d, %d) = %d; want %d", test.l, test.r, test.x, result, test.want)
+		}
 	}
 }
